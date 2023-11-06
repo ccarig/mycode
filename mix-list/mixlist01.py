dc0519efa45e67a5ad093bd2d94ba36e327072b4
ccarig <christopher.carig@tlgcohort.com>
2023-11-06 22:57:36 +0000

    mixed lists

---
+++ mix-list/mixlist01.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# create a list containing three items
+#my_list = [ "192.168.0.5", 5060, "UP" ]
+iplist = [5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
+
+# display the first item in the list
+print("The first ip is: " + iplist[3] )
+
+# display te second item in the list
+print("The second ip is: " + iplist[4] )
+
+# display the third item in the list
+#print("The last item in the list (state): " + my_list[2] )
+
